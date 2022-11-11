3999a76344ab53be147fbf0e4c7dbd4cc3f568d7
gustavoeichler <guga_eichler@hotmail.com>
2022-11-11 15:01:12 -0300

    Ajustes para a conclusão do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone14 = new Iphone("61996060064", "Iphone 14", "ABCD123456",6);
+Nokia nokiaLumia = new Nokia("61996060027","Nokia Lumia", "ABCD123457", 12);
+
+nokiaLumia.Ligar();
+iphone14.ReceberLigacao();
+iphone14.Ligar();
+nokiaLumia.ReceberLigacao();
+
+iphone14.InstalarAplicativo("Sleep Cycle");
+nokiaLumia.InstalarAplicativo("Sleep Cycle");
